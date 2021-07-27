a64125f8e31bf34d54e8aba4cfd9045607ed78ed
Mr Sam Law <sam@chongland.com>
2021-07-27 16:40:38 +1000

    Basic GraphQL running

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id })
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+};
+module.exports = resolvers;
