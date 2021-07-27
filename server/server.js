017e93992a1d5235e6101e4e582465385647fe7d
Mr Sam Law <sam@chongland.com>
2021-07-27 16:00:54 +1000

    Revert

--- server/server.js
+++ server/server.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const path = require('path');
-
-// Apollo Server package
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Define Apollo Server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -28,8 +17,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 API sever running on port ${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
