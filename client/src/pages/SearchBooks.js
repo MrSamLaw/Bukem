5d3338c5cbceb448eb5074c10e1864ae5456e0fe
Mr Sam Law <sam@chongland.com>
2021-07-29 22:14:44 +1000

    SearchBook Saving

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -58,12 +58,10 @@ const SearchBooks = () => {
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
-    console.log(bookId);
+
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => {
-      console.log(book);
-      return book.bookId === bookId;
-    })
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    console.log(bookToSave);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -74,7 +72,7 @@ const SearchBooks = () => {
     try {
       // const response = await saveBook(bookToSave, token);
       await saveBook({
-        variables: { input: bookToSave }
+        variables: { ...bookToSave }
       })
       // if (!response.ok) {
       //   throw new Error('something went wrong!');
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -36,7 +36,7 @@ const resolvers = {
         saveBook: async (parent, { input }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
-                    { _id: context.user.id },
+                    { _id: context.user._id },
                     { $addToSet: { savedBooks: input } },
                     { new: true, runValidators: true }
                 );
